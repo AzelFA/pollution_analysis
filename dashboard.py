149eda2f976f40a710ceb31086ac3697ddb42c65
AzelFA <89203033+AzelFA@users.noreply.github.com>
2025-03-19 23:39:03 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -4,12 +4,10 @@ import seaborn as sns
 import streamlit as st
 from sklearn.preprocessing import MinMaxScaler
 
-
-from babel.numbers import format_currency
 sns.set(style='dark')
 
 #Load dataset
-df = pd.read_csv("./dashboard/shunyi_cleaned.csv")
+df = pd.read_csv("dashboard/shunyi_cleaned.csv")
 
 #Dataset Normalisasi
 scaler = MinMaxScaler()
@@ -18,7 +16,7 @@ normalisasi = df
 normalisasi[polusi] = scaler.fit_transform(df[polusi])
 polusi_trends = normalisasi.groupby("year")[polusi].mean().reset_index()
 
-# Judul Dashboard
+
 st.title("Dashboard Analisis Polusi Udara")
 st.text(
     """
@@ -28,7 +26,7 @@ st.text(
     """
 )
 
-# Tren tahunan polusi udara
+#Tren tahunan polusi udara
 st.subheader("Tren Tahunan Polusi Udara di Shunyi (2013-2017)")
 polusi_trends = df.groupby("year")[polusi].mean().reset_index()
 plt.figure(figsize=(10, 6))
@@ -41,57 +39,13 @@ plt.xticks(polusi_trends['year'])
 plt.legend()
 st.pyplot(plt)
 
-# Scatter plot hubungan curah hujan dengan polutan
-st.subheader("Hubungan antara Curah Hujan dan Polutan")
-col1, col2 = st.columns(2)
-col3, col4 = st.columns(2)
-col5, col6 = st.columns(2)
+#Scatter plot hubungan curah hujan dengan polutan
+st.subheader("Hubungan antara Curah Hujan dengan Polutan")
+select_polusi = st.selectbox("Pilih Polutan:", polusi)
+plt.figure(figsize=(7, 5))
+sns.scatterplot(x=df["RAIN"], y=df[select_polusi], alpha=0.5)
+plt.xlabel("Curah Hujan (mm)")
+plt.ylabel(f"{select_polusi} (µg/m³)" if select_polusi != "CO" else "CO (mg/m³)")
+st.pyplot(plt)
 
-with col1:
-    st.subheader("PM2.5")
-    plt.figure(figsize=(6, 4))
-    sns.scatterplot(x=df["RAIN"], y=df["PM2.5"], alpha=0.5)
-    plt.xlabel("Curah Hujan (mm)")
-    plt.ylabel("PM2.5 (µg/m³)")
-    st.pyplot(plt)
-
-with col2:
-    st.subheader("PM10")
-    plt.figure(figsize=(6, 4))
-    sns.scatterplot(x=df["RAIN"], y=df["PM10"], alpha=0.5)
-    plt.xlabel("Curah Hujan (mm)")
-    plt.ylabel("PM10 (µg/m³)")
-    st.pyplot(plt)
-
-with col3:
-    st.subheader("SO2")
-    plt.figure(figsize=(6, 4))
-    sns.scatterplot(x=df["RAIN"], y=df["SO2"], alpha=0.5)
-    plt.xlabel("Curah Hujan (mm)")
-    plt.ylabel("SO2 (µg/m³)")
-    st.pyplot(plt)
-
-with col4:
-    st.subheader("NO2")
-    plt.figure(figsize=(6, 4))
-    sns.scatterplot(x=df["RAIN"], y=df["NO2"], alpha=0.5)
-    plt.xlabel("Curah Hujan (mm)")
-    plt.ylabel("NO2 (µg/m³)")
-    st.pyplot(plt)
-
-with col5:
-    st.subheader("CO")
-    plt.figure(figsize=(6, 4))
-    sns.scatterplot(x=df["RAIN"], y=df["CO"], alpha=0.5)
-    plt.xlabel("Curah Hujan (mm)")
-    plt.ylabel("CO (mg/m³)")
-    st.pyplot(plt)
-
-with col6:
-    st.subheader("O3")
-    plt.figure(figsize=(6, 4))
-    sns.scatterplot(x=df["RAIN"], y=df["O3"], alpha=0.5)
-    plt.xlabel("Curah Hujan (mm)")
-    plt.ylabel("O3 (µg/m³)")
-    st.pyplot(plt)
 st.caption('Copyright (c) AzelFA 2025')
